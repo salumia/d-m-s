f3b9984ce2acfd0698d29653050701ce7145913d
Nitin <nitinsaluja98@gmail.com>
2019-01-02 17:57:35 +0530

    Integrate latest design

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TemplateConfigService } from "./template/template-config.service";
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,19 @@ import { TemplateConfigService } from "./template/template-config.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-
-  constructor(public template: TemplateConfigService) { }
+	title = 'app';
+  	loggedInUser: any;
+	constructor(public template: TemplateConfigService, private auth: AuthService) {
+		this.template.listen().subscribe((m:any) => {
+            this.refreshStyle();
+        })
+	}
+  	ngOnInit() {
+		this.loggedInUser = this.auth.getAuth();
+		console.log(this.loggedInUser);
+	}
+	
+	refreshStyle() {
+		this.loggedInUser = this.auth.getAuth();
+	}
 }
